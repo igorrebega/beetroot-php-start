f07963f76fbf874e1c62881dc9aae1e27cdbc380
Igor Rebega <igorrebega@gmail.com>
2018-12-04 20:53:19 +0200

    Adding composer

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,14 +1,7 @@
 <?php
 
-require 'functions.php';
-
 $app = [];
 $app['config'] = require 'config.php';
-
-require 'Router.php';
-require 'Request.php';
-require 'database/Connection.php';
-require 'database/QueryBuilder.php';
 
 $app['database'] = new QueryBuilder(
     Connection::make($app['config']['database'])
